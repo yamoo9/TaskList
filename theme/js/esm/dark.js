a635f7995190ed28986e514ae37ef33505a389f7
yamoo9 <yamoo9@naver.com>
2022-08-13 08:44:14 +0000

    transform. create Theme Files & Tailwind CSS

--- theme/js/esm/dark.js
+++ theme/js/esm/dark.js
@@ -1,18 +1,18 @@
 /**
  * Do not edit directly
- * Generated on Wed, 10 Aug 2022 00:46:39 GMT
+ * Generated on Sat, 13 Aug 2022 08:44:09 GMT
  */
 
 export const Fg100 = "#efefef";
 export const FgDefault = "#ffffff";
 export const Bg100 = "#1a1a1a";
 export const BgDefault = "#000000";
-export const Primary100 = "#cde4f4";
-export const Primary200 = "#9cc8e8";
-export const Primary300 = "#6aaddd";
-export const Primary400 = "#3992d2";
-export const Primary500 = "#0776c7";
-export const Primary600 = "#065f9f";
-export const Primary700 = "#044777";
-export const Primary800 = "#032f4f";
-export const Primary900 = "#011828";
+export const Primary100 = "#f9d0d0";
+export const Primary200 = "#f4a1a1";
+export const Primary300 = "#ee7272";
+export const Primary400 = "#e84343";
+export const Primary500 = "#e21414";
+export const Primary600 = "#b51010";
+export const Primary700 = "#880c0c";
+export const Primary800 = "#5b0808";
+export const Primary900 = "#2d0404";
